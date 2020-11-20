7fa36d108191569165869f9471e27d6629bc46e3
DanilaPetrov <isip_d.a.petrov@mpt.ru>
2020-11-20 15:23:56 +0300

    sixth commit

--- app/src/main/java/com/example/practice/curWeatherFragment.kt
+++ app/src/main/java/com/example/practice/curWeatherFragment.kt
@@ -6,6 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.practice.data.OpenWeatherAPI
+import kotlinx.android.synthetic.main.cur_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class curWeatherFragment : Fragment() {
 
@@ -26,6 +31,13 @@ class curWeatherFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val apiService = OpenWeatherAPI()
+
+        GlobalScope.launch(Dispatchers.Main) {                                                        //так делать не надо, ТОЛЬКО ДЛЯ ТЕСТА
+            val currentWeatherJSON = apiService.getCurrentWeather("Moscow").await()
+            txtWeather.text = currentWeatherJSON.toString()
+        }
     }
 
 }
--- app/src/main/java/com/example/practice/data/OpenWeatherAPI.kt
+++ app/src/main/java/com/example/practice/data/OpenWeatherAPI.kt
@@ -1,17 +1,54 @@
 package com.example.practice.data
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 const val API_KEY = "8ab72bdfc20efd0e159b98d001f704b8"
 
-//api.openweathermap.org/data/2.5/weather?q=Moscow&appid=8ab72bdfc20efd0e159b98d001f704b8&Lang=en
+//https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=8ab72bdfc20efd0e159b98d001f704b8&lang=en
 
 interface OpenWeatherAPI {
 
-    @GET("current.json")
+    @GET("weather")
     fun getCurrentWeather(
-        @Query("q") location: String,
-        @Query("lang") languageCode: String = "en"
-    ): CurrentWeatherJSON
+        @Query("q") location: String,  //город
+        @Query("lang") languageCode: String = "ru" //используемый язык
+    ): Deferred<CurrentWeatherJSON>  //выполнение асинхронно
+
+    companion object{  //вместо OpenWeatherAPI.invoke() можно теперь писать только OpenWeatherAPI()
+        operator fun invoke(): OpenWeatherAPI{
+             val requestInterceptor = Interceptor {chain ->    //используем перехватчик, чтобы добавить к ссылке ключ
+                val url = chain.request()  //переходим на ключ
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("appid", API_KEY)
+                    .build()
+                 val request = chain.request()  //копируем ключ
+                     .newBuilder()
+                     .url(url)
+                     .build()
+
+                 return@Interceptor chain.proceed(request)
+             }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory()) //из-за deffered
+                .addConverterFactory(GsonConverterFactory.create())  //конвертирование json в gson, чтобы с ним можно было дальше работать в котлине
+                .build()
+                .create(OpenWeatherAPI::class.java)
+        }
+    }
+
 }
